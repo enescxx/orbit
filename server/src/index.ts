7511f26f4370a5199234beaf1416cfd15046d8b3
enescxx <github@enesc.me>
2026-04-09 23:24:01 +0300

    feat: setup basic express server with typescript

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Chat-App is running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
